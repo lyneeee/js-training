558e5ee14edfd4e4b585667da0ffbd0f223d3e65
lyne <namiss.lyne@gmail.com>
2019-03-31 19:33:03 +0200

    modification tester math pour Travis

--- exercises/max.js
+++ exercises/max.js
@@ -11,6 +11,13 @@
 
 // Your code :
 
+function max(x,y){
+    if (x>y){
+        return(x)
+    }
+    else{return(y)
+    }
+}
 //* Begin of tests
 const assert = require('assert')
 
--- exercises/same-sign.js
+++ exercises/same-sign.js
@@ -9,6 +9,21 @@
 
 // Your code :
 
+function sameSign (x,y) {
+    if ((x>0) && (y>0)){
+        return (true)
+    }
+    else if ((x<0) && (y<0)){
+        return (true)
+    }
+    else if ((x==0) && (y==0)){
+        return (true)
+    }
+    else {
+        return (false)
+    }
+}
+
 //* Begin of tests
 const assert = require('assert')
 
